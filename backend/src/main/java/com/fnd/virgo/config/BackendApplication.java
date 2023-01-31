3dc943ae06460b773f35a04c0768de2fbece755b
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2023-01-31 22:37:52 +0100

    First commit

---
+++ backend/src/main/java/com/fnd/virgo/config/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.fnd.virgo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
+
