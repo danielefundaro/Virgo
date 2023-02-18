13876265e002130db4eb180f9ad944b70ef4f583
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2023-02-18 21:19:41 +0100

    Remove useless code

---
+++ backend/src/main/java/com/fnd/virgo/controller/CommonController.java
@@ -0,0 +1,43 @@
+package com.fnd.virgo.controller;
+
+import com.fnd.virgo.dto.CommonFieldsDTO;
+import com.fnd.virgo.entity.CommonFields;
+import com.fnd.virgo.repository.CommonRepository;
+import com.fnd.virgo.service.CommonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Validated
+public interface CommonController<C extends CommonFields, D extends CommonFieldsDTO, R extends CommonRepository<C>, S extends CommonService<C, D, R>> {
+
+    S getService();
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
+    private List<D> getAll() {
+        return getService().getAll();
+    }
+
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    private D save(@RequestBody D d) {
+        return getService().save(d);
+    }
+
+    @PutMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
+    private D update(@RequestBody D d) {
+        return getService().update(d);
+    }
+
+    @DeleteMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
+    private D delete(@RequestBody D d) {
+        return getService().delete(d);
+    }
+}
--- backend/src/main/java/com/fnd/virgo/repository/NoteRepository.java
+++ backend/src/main/java/com/fnd/virgo/repository/NoteRepository.java
@@ -1,9 +1,6 @@
 package com.fnd.virgo.repository;
 
 import com.fnd.virgo.entity.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
