835899fe9591d31b4f788953b069d6f00a24ffd2
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2023-02-27 02:54:47 +0100

    Introduce keycloak authentication

--- backend/src/main/java/com/fnd/virgo/SecurityConfig.java
+++ backend/src/main/java/com/fnd/virgo/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.fnd.virgo;
 
 
+import com.fnd.virgo.config.KeycloakLogoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,10 +14,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    public SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
 
     @Bean
     PasswordEncoder getEncoder() {
@@ -24,10 +39,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-//                .formLogin((form) -> form.loginPage("/login").permitAll())
-//                .logout(LogoutConfigurer::permitAll);
+        http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/credentials/*").authenticated()
+                .requestMatchers("/notes/*").authenticated()
+                .requestMatchers("/workspaces/*").authenticated()
+                .anyRequest().authenticated().and()
+                .oauth2Login().and().logout().addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/").and()
+                .oauth2ResourceServer().jwt();
 
         return http.build();
     }
