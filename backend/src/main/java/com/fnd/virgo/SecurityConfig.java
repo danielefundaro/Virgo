3ab578645068f5ebcf452bfcecd4bf720d5c64aa
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2023-03-03 01:14:03 +0100

    Remove useless code

--- backend/src/main/java/com/fnd/virgo/SecurityConfig.java
+++ backend/src/main/java/com/fnd/virgo/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.fnd.virgo;
 
 
-import com.fnd.virgo.config.KeycloakLogoutHandler;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -22,44 +15,24 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
-
-    public SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-    }
-
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Bean
-    PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(@NotNull HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/auth/login").permitAll()
                 .requestMatchers("/credentials/*").authenticated()
                 .requestMatchers("/notes/*").authenticated()
                 .requestMatchers("/workspaces/*").authenticated()
-                .anyRequest().permitAll();
-        http.oauth2Login().and().logout().addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/");
+                .requestMatchers("/users/*").authenticated()
+                .anyRequest().authenticated();
+        http.oauth2Login().and().logout();
         http.oauth2ResourceServer().jwt();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder().passwordEncoder(getEncoder()::encode)
-                .username("username")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
     }
 }
