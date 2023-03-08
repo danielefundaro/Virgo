cc97d0552b6fa1ad9a90f1a7ed0a97c4d3c5ed83
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2023-03-09 00:53:33 +0100

    Add vault component

---
+++ frontend/src/app/auth/pages/vault/vault.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'vault',
+    templateUrl: './vault.component.html',
+    styleUrls: ['./vault.component.scss']
+})
+export class VaultComponent {
+
+}
