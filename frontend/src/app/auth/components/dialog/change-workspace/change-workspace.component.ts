15116f725243292c49f5791354c6bc082dc1a1c2
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2023-03-26 21:24:11 +0200

    Mandatory workspace

--- frontend/src/app/auth/components/dialog/change-workspace/change-workspace.component.ts
+++ frontend/src/app/auth/components/dialog/change-workspace/change-workspace.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { firstValueFrom } from 'rxjs';
-import { IChangeWorkspaceRequest, IChangeWorkspaceResponse, Workspace } from 'src/app/auth/models';
+import { IChangeWorkspaceRequest, Workspace } from 'src/app/auth/models';
 import { WorkspacesService } from 'src/app/auth/services';
 
 @Component({
@@ -15,7 +15,7 @@ export class ChangeWorkspaceComponent {
     public workspaces!: Workspace[];
     public workspaceControl!: FormControl;
 
-    constructor(public dialogRef: MatDialogRef<ChangeWorkspaceComponent, IChangeWorkspaceResponse>, private workspacesService: WorkspacesService,
+    constructor(public dialogRef: MatDialogRef<ChangeWorkspaceComponent, Workspace>, private workspacesService: WorkspacesService,
         @Inject(MAT_DIALOG_DATA) public data: IChangeWorkspaceRequest) {
         this.workspaceControl = new FormControl(null);
 
@@ -23,9 +23,5 @@ export class ChangeWorkspaceComponent {
             this.workspaces = workspaces;
             this.workspaceControl = new FormControl(workspaces.find(workspace => workspace.id === data.workspace.id));
         });
-    }
-
-    public close(move: boolean): void {
-        this.dialogRef.close({ move: move, workspace: this.workspaceControl.value });
     }
 }
--- frontend/src/app/auth/models/iChangeWorkspace.ts
+++ frontend/src/app/auth/models/iChangeWorkspace.ts
@@ -4,8 +4,3 @@ export interface IChangeWorkspaceRequest {
     title: string;
     workspace: Workspace;
 }
-
-export interface IChangeWorkspaceResponse {
-    move: boolean;
-    workspace: Workspace;
-}
